98ddc6afc4a42d2dcd5204ef9ff4007b1b93c28a
Chen Kang Yang <30970698+chen-ky@users.noreply.github.com>
2022-01-14 14:59:38 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # term-colour-rs
 Terminal text colour library for Rust
+
+## Using this library
+
+1. Navigate to your project's `Cargo.toml` file.
+2. Insert `term_colour = { git = "https://github.com/chen-ky/term-colour-rs" }` into the `dependencies section`
---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+#![allow(dead_code)]
+
+pub const BLACK                 : &str = "\033[0;30m";
+pub const BLACK_BOLD            : &str = "\033[1;30m";
+
+pub const RED                   : &str = "\033[0;31m";
+pub const RED_BOLD              : &str = "\033[1;31m";
+
+pub const GREEN                 : &str = "\033[0;32m";
+pub const GREEN_BOLD            : &str = "\033[1;32m";
+
+pub const YELLOW                : &str = "\033[0;33m";
+pub const YELLOW_BOLD           : &str = "\033[1;33m";
+
+pub const BLUE                  : &str = "\033[0;34m";
+pub const BLUE_BOLD             : &str = "\033[1;34m";
+
+pub const MAGENTA               : &str = "\033[0;35m";
+pub const MAGENTA_BOLD          : &str = "\033[1;35m";
+
+pub const CYAN                  : &str = "\033[0;36m";
+pub const CYAN_BOLD             : &str = "\033[1;36m";
+
+pub const WHITE                 : &str = "\033[0;37m";
+
+pub const GRAY                  : &str = "\033[0;90m";
+pub const GRAY_BOLD             : &str = "\033[1;90m";
+
+pub const BRIGHT_RED            : &str = "\033[0;91m";
+pub const BRIGHT_RED_BOLD       : &str = "\033[1;91m";
+
+pub const BRIGHT_GREEN          : &str = "\033[0;92m";
+pub const BRIGHT_GREEN_BOLD     : &str = "\033[1;92m";
+
+pub const BRIGHT_YELLOW         : &str = "\033[0;93m";
+pub const BRIGHT_YELLOW_BOLD    : &str = "\033[1;93m";
+
+pub const BRIGHT_BLUE           : &str = "\033[0;94m";
+pub const BRIGHT_BLUE_BOLD      : &str = "\033[1;94m";
+
+pub const BRIGHT_MAGENTA        : &str = "\033[0;95m";
+pub const BRIGHT_MAGENTA_BOLD   : &str = "\033[1;95m";
+
+pub const BRIGHT_CYAN           : &str = "\033[0;96m";
+pub const BRIGHT_CYAN_BOLD      : &str = "\033[1;96m";
+
+pub const BRIGHT_WHITE          : &str = "\033[0;97m";
+pub const BRIGHT_WHITE_BOLD     : &str = "\033[1;97m";
+
+pub const NO_COLOUR             : &str = "\033[0m";
